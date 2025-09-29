6444bc42fb0a8d2f176482b796660a921ca51ad4
Alexander <begemott4@gmail.com>
2025-09-29 07:22:32 +0300

    Updates

--- assets/index-ClcivW_t.js
+++ assets/index-ClcivW_t.js
@@ -1,4 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const t of s.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&n(t)}).observe(document,{childList:!0,subtree:!0});function c(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=c(e);fetch(e.href,s)}})();const d=`<header class="header">\r
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))n(s);new MutationObserver(s=>{for(const e of s)if(e.type==="childList")for(const a of e.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(s){const e={};return s.integrity&&(e.integrity=s.integrity),s.referrerPolicy&&(e.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?e.credentials="include":s.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function n(s){if(s.ep)return;s.ep=!0;const e=r(s);fetch(s.href,e)}})();const d=`<header class="header">\r
     <div class="container">\r
         <div class="header__container">\r
             <img src="/icons/iconLogo.svg" alt="Лого" class="header__logo">\r
@@ -23,24 +23,24 @@
         </nav>\r
     </div>\r
 </header>\r
-`;function u(){const a="/van-gogh-landing/",r=document.createElement("div"),c=d.replaceAll("/icons/",`${a}icons/`);r.innerHTML=c.trim();const n=r.firstElementChild,e=n.querySelector(".header__toggle"),s=n.querySelector(".header__nav--mobile");n.querySelectorAll(".header__nav--mobile .header__link");function t(){n.classList.add("open"),getComputedStyle(s).display!=="none"&&(s.style.maxHeight=s.scrollHeight+"px")}function l(){n.classList.remove("open"),s.style.maxHeight="0px"}return e.addEventListener("click",()=>{n.classList.contains("open")?l():t()}),window.addEventListener("resize",()=>{window.innerWidth>1160?(n.classList.remove("open"),s.style.maxHeight=null):n.classList.contains("open")&&(s.style.maxHeight=s.scrollHeight+"px")}),n}const _=`<section class="products section">\r
+`;function u(){const c="/van-gogh-landing/",t=document.createElement("div"),r=d.replaceAll("/icons/",`${c}icons/`);t.innerHTML=r.trim();const n=t.firstElementChild,s=n.querySelector(".header__toggle"),e=n.querySelector(".header__nav--mobile");n.querySelectorAll(".header__nav--mobile .header__link");function a(){n.classList.add("open"),getComputedStyle(e).display!=="none"&&(e.style.maxHeight=e.scrollHeight+"px")}function i(){n.classList.remove("open"),e.style.maxHeight="0px"}return s.addEventListener("click",()=>{n.classList.contains("open")?i():a()}),window.addEventListener("resize",()=>{window.innerWidth>1160?(n.classList.remove("open"),e.style.maxHeight=null):n.classList.contains("open")&&(e.style.maxHeight=e.scrollHeight+"px")}),n}const _=`<section class="products section">\r
     <div class="container products__container">\r
         <h2 class="products__title">РЕКОМЕНДУЕМОЕ ОБОРУДОВАНИЕ</h2>\r
         <div class="products__grid"></div>\r
         <button class="btn catalog__btn">Перейти в каталог</button>\r
     </div>\r
 </section>\r
-`;function m(a=[]){const r=document.createElement("div");r.innerHTML=_.trim();const c=r.firstChild,n=c.querySelector(".products__grid");return a.forEach((e,s)=>{const t=document.createElement("div");t.className="product-card",(s===2||s===4)&&t.classList.add("product-card--mobile-hidden"),t.innerHTML=`
+`;function g(c=[]){const t="/van-gogh-landing/",r=document.createElement("div");r.innerHTML=_.trim();const n=r.firstChild,s=n.querySelector(".products__grid");return c.forEach((e,a)=>{const i=document.createElement("div");i.className="product-card",(a===2||a===4)&&i.classList.add("product-card--mobile-hidden"),i.innerHTML=`
             <img src="${e.img}" alt="${e.name}" class="product-card__img">
             <div class="product-card__desc-wrapper">
                 <p class="product-card__desc">${e.name}</p>
                 <span class="product-card__status ${e.statusClass}">
-                <img src="/icons/${e.statusClass==="in-stock"?"iconGreenCheck":"iconOrangeCheck"}.svg" alt="" class="product-card__status-icon">
+              <img src="${t}icons/${e.statusClass==="in-stock"?"iconGreenCheck":"iconOrangeCheck"}.svg" alt="" class="product-card__status-icon">
                      ${e.status}
                     </span>
                 <button class="btn card__btn">Подробнее</button>
             </div>
-        `,n.appendChild(t)}),c}const g=`<section class="about section">\r
+        `,s.appendChild(i)}),n}const m=`<section class="about section">\r
     <div class="container">\r
         <h2 class="about__title">О КОМПАНИИ</h2>\r
         <div class="about__container">\r
@@ -80,4 +80,4 @@
         </div>\r
     </div>\r
 </section>\r
-`;function p(){const a=document.createElement("section");return a.className="about section",a.innerHTML=g.trim(),a}const o=document.getElementById("app"),i="/van-gogh-landing/",h=[{img:`${i}images/products1.webp`,name:"Стальной канат для лифта GRS 9X21F(9Х17)-IWRC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${i}images/products2.webp`,name:"Плоский тяговый ремень для лифта GRS AAA717X1 (12 кордов)",status:"Под заказ",statusClass:"out-of-stock"},{img:`${i}images/products3.webp`,name:"Стальной канат для лифта GRS 8X19S-NFC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${i}images/products4.webp`,name:"Стальной канат для лифта GRS 9X21F(9Х17)-IWRC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${i}images/products5.webp`,name:"Стальной канат для лифта GRS 8X19W-CWC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${i}images/products6.webp`,name:"Купить плоский тяговый ремень для лифта GRS AAA717AJ1(8 кордов)",status:"В наличии 50 м.",statusClass:"in-stock"}];o.appendChild(u());o.appendChild(m(h));o.appendChild(p());
+`;function h(){const c="/van-gogh-landing/",t=document.createElement("section"),r=m.replaceAll("/images/",`${c}images/`).replaceAll("/icons/",`${c}icons/`);return t.className="about section",t.innerHTML=r.trim(),t}const l=document.getElementById("app"),o="/van-gogh-landing/",p=[{img:`${o}images/products1.webp`,name:"Стальной канат для лифта GRS 9X21F(9Х17)-IWRC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${o}images/products2.webp`,name:"Плоский тяговый ремень для лифта GRS AAA717X1 (12 кордов)",status:"Под заказ",statusClass:"out-of-stock"},{img:`${o}images/products3.webp`,name:"Стальной канат для лифта GRS 8X19S-NFC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${o}images/products4.webp`,name:"Стальной канат для лифта GRS 9X21F(9Х17)-IWRC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${o}images/products5.webp`,name:"Стальной канат для лифта GRS 8X19W-CWC 1570",status:"В наличии 50 м.",statusClass:"in-stock"},{img:`${o}images/products6.webp`,name:"Купить плоский тяговый ремень для лифта GRS AAA717AJ1(8 кордов)",status:"В наличии 50 м.",statusClass:"in-stock"}];l.appendChild(u());l.appendChild(g(p));l.appendChild(h());
